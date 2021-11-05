5385547e529d40864c4cf3531b889cf5a6fcffe0
nordwaldberg <nord.waldberg@gmail.com>
2021-11-05 18:39:41 +0300

    Initial commit: first version of solution was added

---
+++ src/set-tests.js
@@ -0,0 +1,80 @@
+import  MySet  from './set.js';
+
+const testArr = [1, 1, 2, 3, 4, 5, 5, 6];
+
+console.log(`Initialization of set ${testArr} :`);
+console.log(' ');
+
+const example = new MySet(testArr);
+console.log([...example]); // [ 1, 2, 3, 4, 5, 6 ]
+
+console.log(' ');
+console.log('set size:');
+console.log(' ');
+
+console.log(example.size) //6
+
+console.log(' ');
+console.log('for...of :');
+console.log(' ');
+
+for (const item of example) {
+    console.log(item); // 1 2 3 4 5 6
+}
+console.log(' ');
+console.log('for...of with entries():');
+console.log(' ');
+
+for (const item of example.entries()) {
+    console.log(item); // [ 1, 1 ] [ 2, 2 ] ...
+}
+console.log(' ');
+console.log('set size after clear():');
+console.log(' ');
+
+example.clear();
+console.log(example.size); // 0
+
+console.log(' ');
+console.log('adding object and data, chaining add object:');
+console.log(' ');
+
+const object = {
+    getValue () { return this.value }
+}
+
+const data = {
+    value: 42
+}
+
+example.add(object);
+example.add(data);
+
+
+example.add(object).add(object).add(object);
+console.log([...example]) // [ 1, 2, 3, 4, 5, 6, {}, {}, {}, {}, {} ]
+console.log(' ');
+
+console.log('deleting data and has({}), has(object), has(data)');
+console.log(' ');
+example.delete(data);
+
+console.log(example.has({})); // false
+console.log(example.has(object)); // true
+console.log(example.has(data)); // false
+
+console.log(' ');
+console.log('logging tag');
+console.log(' ');
+
+console.log(example === example.valueOf()) // true
+console.log(String(example)) // [object ^_^]
+console.log(Object.prototype.toString.call(example)) // [object ^_^]
+
+console.log(' ');
+console.log('strange forEach :)');
+console.log(' ');
+
+example.forEach(function (item) {
+    console.log(item.getValue.call(this)); // 42
+}, data)
---
+++ src/set.js
@@ -0,0 +1,90 @@
+export default class MySet {
+    #data = [];
+
+    constructor(arr) {
+        if (arr) {
+            arr.forEach((value) => this.add(value));
+        }
+    }
+
+    add(value) {
+        if (!this.#data.includes(value)) {
+            this.#data.push(value);
+        }
+        return this;
+    }
+
+    clear() {
+        this.#data = [];
+    }
+
+    delete(value) {
+        let found = false;
+        this.#data = this.#data.filter((ownValue) => {
+            if (ownValue !== value) {
+                return true;
+            } else {
+                found = true;
+                return false;
+            }
+        });
+        return found;
+    }
+
+    has(value) {
+        return this.#data.includes(value);
+    }
+
+    get size() {
+        return this.#data.length;
+    }
+
+    [Symbol.iterator]() {
+        const last = {
+            done: true,
+            value: null,
+        };
+        if (this.#data.length === 0) {
+            return {
+                next() {
+                    return last;
+                },
+            };
+        }
+        return {
+            current: 0,
+            data: this.#data,
+            next() {
+                if (this.current === this.data.length) {
+                    return last;
+                }
+                const value = this.data[this.current];
+                this.current++;
+                return {
+                    done: false,
+                    value,
+                };
+            },
+        };
+    }
+
+    get [Symbol.toStringTag]() {
+        return '^_^';
+    }
+
+    forEach(callback, context) {
+        Array.prototype.forEach.call(this.#data, callback, context);
+    }
+
+    values() {
+        return this.#data;
+    }
+
+    keys() {
+        return this.#data;
+    }
+
+    entries() {
+        return this.#data.map(value => [value, value]);
+    }
+}
